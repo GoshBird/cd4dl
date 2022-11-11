f73b27881c56fba806177a7513175cf0bc4a64b4
Takagoshi <54761038+GoshBird@users.noreply.github.com>
2022-11-11 17:52:04 +0900

    activate msec

--- timer.js
+++ timer.js
@@ -1,6 +1,8 @@
+const day = document.getElementById("day");
 const hour = document.getElementById("hour");
 const min = document.getElementById("min");
 const sec = document.getElementById("sec");
+const msec = document.getElementById("msec");
 
 function countdown() {
     const now = new Date(); // 現在時刻を取得
@@ -12,12 +14,14 @@ function countdown() {
     const calcHour = Math.floor(diff / 1000 / 60 / 60) % 24;
     const calcMin = Math.floor(diff / 1000 / 60) % 60;
     const calcSec = Math.floor(diff / 1000) % 60;
+    const calcmSec = Math.floor(diff / 10)% 100;
 
     // 取得した時間を表示（2桁表示
     day.innerHTML = calcDay < 10 ? '0' + calcDay : calcDay;
     hour.innerHTML = calcHour < 10 ? '0' + calcHour : calcHour;
     min.innerHTML = calcMin < 10 ? '0' + calcMin : calcMin;
     sec.innerHTML = calcSec < 10 ? '0' + calcSec : calcSec;
+    msec.innerHTML = calcmSec < 10 ? '0' + calcmSec : calcmSec;
 }
 countdown();
-setInterval(countdown,1000);
+setInterval(countdown,10);
